f9933e8a88f1b85ad4b985541e974c0f384d6f16
Aneesh Raman <aneeshraman@Aneeshs-MacBook-Pro.local>
2020-08-28 18:35:09 +0530

    picking_numbers update

--- Hackerrank/picking_numbers.py
+++ Hackerrank/picking_numbers.py
@@ -1,29 +1,12 @@
 class PickingNumbers:
+
     def __init__(self, a, n):
-        self.indexList = []
-        self.differenceArray = []
         self.array = a
         self.number_of_elements = n
-
+    
     def difference_array(self):
+        self.differenceArray = []
         self.array.sort(reverse=True)
-        indexNumber = -1
-        for i in self.array:
-            indexNumber += 1
-            y = self.array[indexNumber + 1]
-            self.differenceArray.append(y - i)
-
-    def analyse(self):
-        indexNumber = -1
-        for i in self.differenceArray:
-            indexNumber += 1
-            if i == 1 or i == 0:
-                self.indexList.append(indexNumber)
-                self.indexList.append(indexNumber + 1)
-
-        self.indexList = list(dict.fromkeys(self.indexList))
-
-        print(len(self.indexList))
 
 
 n = int(input().strip())
@@ -31,5 +14,3 @@ n = int(input().strip())
 a = list(map(int, input().rstrip().split()))
 
 result = PickingNumbers(a, n)
-result.difference_array()
-result.analyse()
