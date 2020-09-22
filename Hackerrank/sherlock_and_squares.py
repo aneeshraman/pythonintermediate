bccd03e251bcb74af01b7a7fed590ff0d90323d3
Aneesh Raman <aneeshraman17mar@gmail.com>
2020-09-22 21:27:59 +0530

    Updated sherlock_and_squares.py

--- Hackerrank/sherlock_and_squares.py
+++ Hackerrank/sherlock_and_squares.py
@@ -4,9 +4,18 @@ import math
 def squares(a, b):
     string_list = []
     range_list = []
+    final_num = 0
     for range_element in range(a, b + 1):
         string_list.append(str(math.sqrt(range_element)))
         range_list.append(range_element)
+
+    index_num = -1
+    for string_list_element in string_list:
+        index_num += 1
+        if len(string_list_element) == 3:
+            final_num += 1
+
+    print(final_num)
 
 
 q = int(input())
